43f41d297d35abb21d28100f0329db0b52b410df
Mikolaj Dawidowski ubuntu2 <dawmster@gmail.com>
2021-10-08 17:35:22 +0200

    blkXXX.dat files can be moved to other drive and be symlinked from there

--- src/fs.cpp
+++ src/fs.cpp
@@ -1,15 +1,30 @@
 #include "fs.h"
 
 #include <boost/filesystem.hpp>
+#include <iostream>
 
 namespace fsbridge {
 
 FILE *fopen(const fs::path &p, const char *mode) {
-    return ::fopen(p.string().c_str(), mode);
+    fs::path real_path = p;
+    if( fs::is_symlink(p) ){
+        real_path = fs::read_symlink(p);
+        fprintf(stdout, "Is symlink:%s\n",real_path.string().c_str());
+        fprintf(stderr, "Is symlink:%s\n",real_path.string().c_str());
+        // std::cout << "Is symlink:" << p << " -> " << real_path << "\n";
+        // std::cerr << "Is symlink:" << p << " -> " << real_path << "\n";
+    }
+    return ::fopen(real_path.string().c_str(), mode);
 }
 
 FILE *freopen(const fs::path &p, const char *mode, FILE *stream) {
-    return ::freopen(p.string().c_str(), mode, stream);
+    fs::path real_path = p;
+    if( fs::is_symlink(p) ){
+        real_path = fs::read_symlink(p);
+        fprintf(stdout, "Is symlink fropen:%s\n",real_path.string().c_str());
+        fprintf(stderr, "Is symlink fropen:%s\n",real_path.string().c_str());
+    }
+    return ::freopen(real_path.string().c_str(), mode, stream);
 }
 
 } // fsbridge
