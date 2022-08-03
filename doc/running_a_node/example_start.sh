3b4f2170d7b2ce72388b4da21bd2f78c9a0f2426
dawmster <dawmster@gmail.com>
2022-08-03 10:54:28 +0200

    new configurations

--- doc/running_a_node/example_start.sh
+++ doc/running_a_node/example_start.sh
@@ -1,1 +1,4 @@
-~/bitcoin/bin/bitcoind -conf=/media/mikolaj/bitcoin/bitcoin.conf -datadir=/media/mikolaj/bitcoin/bitcoin-data -txindex -maxuploadtarget=10 -timeout=10000 -rest
+#~/bitcoin/bin/bitcoind_mik -conf=/media/mikolaj/bitcoin/bitcoin.conf -datadir=/media/mikolaj/bitcoin/bitcoin-data -daemon -txindex -maxuploadtarget=10 -timeout=10000 -rest
+
+~/bitcoin/bin/bitcoind-symlink.1.0.10 -conf=/media/mikolaj/bitcoin/bitcoin.conf -datadir=/media/mikolaj/bitcoin/bitcoin-data -txindex -maxuploadtarget=10 -minminingtxfee=0.00000001 -timeout=10000 -rest
+#https://github.com/bitcoin/bitcoin/blob/master/doc/REST-interface.md
---
+++ doc/running_a_node/notxindexstart.sh
@@ -0,0 +1,4 @@
+#~/bitcoin/bin/bitcoind-symlink.1.0.10 -conf=/media/mikolaj/bitcoin/bitcoin.conf -datadir=/media/mikolaj/bitcoin/bitcoin-data -daemon -peertimeout=1200
+#bitcoind-symlink.1.0.10 -conf=/media/mikolaj/bitcoin/bitcoin.conf -datadir=/media/mikolaj/bitcoin/bitcoin-data -txindex -maxuploadtarget=10 -minminingtxfee=0.00000001 -timeout=10000 -rest
+
+~/bitcoin/bin/bitcoind-symlink.1.0.10 -conf=/media/mikolaj/bitcoin/bitcoin.conf -datadir=/media/mikolaj/bitcoin/bitcoin-data -daemon -minminingtxfee=0.00000001
